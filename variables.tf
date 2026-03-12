6f89c0dcfa0c6dfcb300010a8c07c580266d2466
krishnakala987-byte <krishnakala987@gmail.com>
2026-03-12 18:48:51 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region where infrastructure will be deployed"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version used for the EKS cluster"
+  type        = string
+  default     = "1.27"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC network"
+  type        = string
+  default     = "10.0.0.0/16"
+}
